e39ecbe401a296db9bca732a23ae4581dff7e4a3
Bryson McClure <brysonmcclure14@gmail.com>
2022-10-30 11:02:53 -0400

    Added support for finding a letter by its ID for individual letter views

--- controllers/letters-controller.js
+++ controllers/letters-controller.js
@@ -1,10 +1,12 @@
 import letterDao from "../mongoManagment/lettersMDS/letter-dao.js";
+import profile from "../controllers/authentication-controller.js"
 
 const lettersController = (app) => {
     app.get('/api/letters', findAllLetters);
     //Not sure on this one yet, how do key value pairs over just path params? Would that be better?
     app.get('/api/letters/:attribute/:value', findLettersByAttribute);
-    app.post('/api/letters', createLetter);
+    app.get('/api/letters/:lid', findLetterById);
+    app.post('/api/letters/:aid', createLetter);
     app.delete('/api/letters/:lid', deleteLetter);
     app.put('/api/letters/:lid', updateLetter);
 }
@@ -19,8 +21,19 @@ const findLettersByAttribute = async (req, res) => {
     res.send(200);
 }
 
+const findLetterById = async (req, res) => {
+    const letterId = req.params.lid;
+    const letter = await letterDao.findLetterById(letterId);
+    res.json(letter);
+}
+
 const createLetter = async (req, res) => {
     const newLetter = req.body;
+    //getting passed an author id now by the service. Is this better?
+    newLetter.author = req.params['aid'];
+    // //Is this an ok practice calling on other controllers like this to access the session?
+    // const authoringUser = await profile();
+    // newLetter.author = authoringUser._id;
     const insertedLetter = await letterDao.createLetter(newLetter);
     res.json(insertedLetter);
 }
--- mongoManagment/lettersMDS/letter-dao.js
+++ mongoManagment/lettersMDS/letter-dao.js
@@ -1,8 +1,26 @@
 import lettersModel from "./letter-model.js";
 
-export const findAllLetters = () => lettersModel.find();
+export const findAllLetters = () => lettersModel.find().populate('author');
 export const findLettersByAttribute = (attribute, value) => lettersModel.find({attribute: value});
-export const createLetter = (newLetter) => lettersModel.create(newLetter);
+export const findLetterById = async (lid) => {
+    //difference between find and findone is just find the first instance vs all matching instances maybe me thinks??
+    const response = await lettersModel.findOne({_id: lid}).populate('author');
+    return response;
+}
+export const createLetter = async (newLetter) => {
+    //Setting default values
+    newLetter.likes = 0;
+    newLetter.dislikes = 0;
+    newLetter.date = new Date();
+    //Theoretically and in it should pratcie, creating a letter with an undefined author is an impossibble case we should not need to cover
+    //Should be an implied return since create call returns something. Populate operates on return value, not
+    //On create method itself thus not affecting/effecting what gets added to the data base, just what our
+    //controller reses back to the client/ which what/ what which ultimately ends up in the reducer, which
+    //was causing the error I believe
+    const response = await lettersModel.create(newLetter);
+    const returnValue = await  response.populate('author');
+    return returnValue;
+}
 export const deleteLetter = (lid) => lettersModel.deleteOne({_id: lid});
 export const updateLetter = (lid, letter) => lettersModel.updateOne({_id: lid}, {$set: letter});
-export default {findAllLetters, findLettersByAttribute, createLetter, deleteLetter, updateLetter};
+export default {findAllLetters, findLettersByAttribute, findLetterById, createLetter, deleteLetter, updateLetter};
